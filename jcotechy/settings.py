fc4f4008d6b57ae0807b9c3b024bb264052ffcd5
Joshua Odigbo <59263123+Joshuasco@users.noreply.github.com>
2023-04-15 19:09:06 +0100

    Update settings.py

--- jcotechy/settings.py
+++ jcotechy/settings.py
@@ -112,7 +112,7 @@ WSGI_APPLICATION = 'jcotechy.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-if DEBUG:
+if not DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
